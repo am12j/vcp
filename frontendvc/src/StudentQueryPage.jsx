a6b24d167376890d1614b9946d5a1860423061f8
md aman khan <926416016aman@gmail.com>
2026-01-01 03:38:57 +0530

    the fetch function is updated  succesfully

--- frontendvc/src/StudentQueryPage.jsx
+++ frontendvc/src/StudentQueryPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
-const socket = io("http://localhost:5000");
+const socket = io("https://vcp-rs8t.onrender.com");
 const pageStyle = {
   minHeight: "100vh",
   display: "flex",
--- frontendvc/src/TeacherQueryPage.jsx
+++ frontendvc/src/TeacherQueryPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://vcp-rs8t.onrender.com");
 
 function TeacherQueryPage() {
   const [formData, setFormData] = useState({ query: "" });
